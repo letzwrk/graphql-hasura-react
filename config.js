c0e64538d2e78853ff7d38b1d8dd346523f02745
Everyone <91630063+letzwrk@users.noreply.github.com>
2022-05-03 22:39:38 -0500

    config copy paste

--- config.js
+++ config.js
@@ -5,25 +5,23 @@ const config = {
     gaTrackingId: null,
     trailingSlash: false,
   },
-  
-  header: {   
-    logo: 'https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/brand.svg', 
-    logoLink: 'https://hasura.io/learn/',   
-    title: 
-      "<a href='https://hasura.io/learn/'><img class='img-responsive' src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/learn-logo.svg' alt='Learn logo' /></a>"  ,
-
-    githubUrl: 'https://github.com/letzwrk',
+  header: {
+    logo: 'https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/brand.svg',
+    logoLink: 'https://hasura.io/learn/',
+    title:
+      "<a href='https://hasura.io/learn/'><img class='img-responsive' src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/learn-logo.svg' alt='Learn logo' /></a>",
+    githubUrl: 'https://github.com/hasura/gatsby-gitbook-boilerplate',
     helpUrl: '',
     tweetText: '',
     social: `<li>
-		    <a href="https://twitter.com/letz_wrk" target="_blank" rel="noopener">
+		    <a href="https://twitter.com/hasurahq" target="_blank" rel="noopener">
 		      <div class="twitterBtn">
 		        <img src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/twitter-brands-block.svg' alt={'Twitter'}/>
 		      </div>
 		    </a>
 		  </li>
 			<li>
-		    <a href="https://discordapp.com/invite/" target="_blank" rel="noopener">
+		    <a href="https://discordapp.com/invite/hasura" target="_blank" rel="noopener">
 		      <div class="discordBtn">
 		        <img src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/discord-brands-block.svg' alt={'Discord'}/>
 		      </div>
@@ -40,32 +38,30 @@ const config = {
   },
   sidebar: {
     forcedNavOrder: [
-      '/payments', // add trailing slash if enabled above
+      '/introduction', // add trailing slash if enabled above
       '/codeblock',
     ],
     collapsedNav: [
       '/codeblock', // add trailing slash if enabled above
     ],
-    /*  BENEITH LOGO ORIGINALLY 
     links: [{ text: 'Hasura', link: 'https://hasura.io' }],
     frontline: false,
     ignoreIndex: true,
     title:
-      "<a href='https://hasura.io/learn/'>graphql </a><div class='greenCircle'></div><a href='https://hasura.io/learn/graphql/react/introduction/'>react</a>",  */
-    },
-
+      "<a href='https://hasura.io/learn/'>graphql </a><div class='greenCircle'></div><a href='https://hasura.io/learn/graphql/react/introduction/'>react</a>",
+  },
   siteMetadata: {
-    title: 'LetzWrk repo can be found here',
-    description: 'Documentation built with mdx.',
+    title: 'Gatsby Gitbook Boilerplate | Hasura',
+    description: 'Documentation built with mdx. Powering hasura.io/learn ',
     ogImage: null,
-    docsLocation: 'https://github.com/letzwrk/letzwrk.github.io/tree/master/content',
+    docsLocation: 'https://github.com/hasura/gatsby-gitbook-boilerplate/tree/master/content',
     favicon: 'https://graphql-engine-cdn.hasura.io/img/hasura_icon_black.svg',
   },
   pwa: {
     enabled: false, // disabling this will also remove the existing service worker.
     manifest: {
-      name: 'graphql-hasura-react',
-      short_name: 'Handyman Site',
+      name: 'Gatsby Gitbook Starter',
+      short_name: 'GitbookStarter',
       start_url: '/',
       background_color: '#6b37bf',
       theme_color: '#6b37bf',
