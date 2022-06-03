c4e8d09b274fe62fcae6bc8f7bf173ffb08c0593
Everyone <91630063+letzwrk@users.noreply.github.com>
2022-06-02 20:58:26 -0500

    first commit

--- gatsby-node.js
+++ gatsby-node.js
@@ -108,3 +108,38 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
     });
   }
 };
+
+
+/*
+Often you will need to programmatically create pages. For example, you have markdown files where each should be a page.
+
+This example assumes that each markdown page has a path set in the frontmatter of the markdown file.
+*/
+
+const path = require("path")
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const queryResults = await graphql(`
+    query AllProducts {
+      allProducts {
+        nodes {
+          id
+          name
+          price
+          description
+        }
+      }
+    }
+  `)
+  const productTemplate = path.resolve(`src/templates/product.js`)
+  queryResults.data.allProducts.nodes.forEach(node => {
+    createPage({
+      path: `/products/${node.id}`,
+      component: productTemplate,
+      context: {
+        // This time the entire product is passed down as context
+        product: node,
+      },
+    })
+  })
+}
