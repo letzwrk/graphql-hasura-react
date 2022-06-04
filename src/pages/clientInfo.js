efbced80be3765b242a9391d1e084d7e37e64304
Everyone <91630063+letzwrk@users.noreply.github.com>
2022-06-03 19:24:50 -0500

    product.js

---
+++ src/pages/clientInfo.js
@@ -0,0 +1,38 @@
+
+
+/*
+
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { useSelector } from "react-redux";
+import { useSnackbar } from "notistack";
+import { useHistory } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { useMutation } from "@apollo/react-hooks";
+import { useParams } from "react-router-dom";
+import { useSnackbarContext } from "../../../context/SnackbarContext";
+import { useAuthContext } from "../../../context/AuthContext";
+import { useClientContext } from "../../../context/ClientContext";
+import { useClientInfoContext } from "../../../context/ClientInfoContext";
+import { useClientInfoMutation } from "../../../graphql/clientInfo.graphql";
+import { useClientInfoQuery } from "../../../graphql/clientInfo.graphql";
+import { useClientInfoUpdateMutation } from "../../../graphql/clientInfo.graphql";
+import { useClientInfoUpdateSubscription } from "../../../graphql/clientInfo.graphql";
+import { useClientInfoUpdateSubscriptionQuery } from "../../../graphql/clientInfo.graphql";
+import { useClientInfoUpdateSubscriptionQueryVariables } from "../../../graphql/clientInfo.graphql";
+import { useClientInfoUpdateSubscriptionQuery_clientInfoUpdateSubscription_clientInfo } from "../../../graphql/clientInfo.graphql";
+import { useClientInfoUpdateSubscriptionQuery_clientInfoUpdateSubscription_clientInfo_client } from "../../../graphql/clientInfo.graphql";
+import { useClientInfoUpdateSubscriptionQuery_clientInfoUpdateSubscription_clientInfo_client_clientInfo } from "../../../graphql/clientInfo.graphql";
+
+export const ClientInfo = () => {
+}
+export default ClientInfo;
+
+export const ClientInfoForm = () => {
+}
+export default ClientInfoForm;
+
+export const ClientInfoForm = () => {
+}
+
+*/
